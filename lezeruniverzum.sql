820fc533409ee7031aba9c2e059f85166b7f139b
KZsolt <kukovecz.zsolt@gmail.com>
2023-03-28 17:17:24 +0200

    sql 2.0
    Átalakítottam 1-2 nevet + hozzá adtam pár dolgot amit még kihagytam

--- lezeruniverzum.sql
+++ lezeruniverzum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 27, 2023 at 07:43 PM
+-- Generation Time: Mar 28, 2023 at 05:16 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -28,12 +28,14 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `felhasznalok` (
-  `felhaszNev` varchar(20) NOT NULL,
-  `felhaszId` int(11) NOT NULL,
-  `felhaszJelszo` varchar(20) NOT NULL,
-  `felhaszEmail` varchar(255) NOT NULL,
-  `felhaszKnev` varchar(255) NOT NULL,
-  `felhaszVnev` varchar(255) NOT NULL
+  `ID` int(11) NOT NULL,
+  `Jelszo` varchar(20) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Knev` varchar(255) NOT NULL,
+  `Vnev` varchar(255) NOT NULL,
+  `Varos` varchar(255) NOT NULL,
+  `Iranyitoszam` int(4) NOT NULL,
+  `Lakcim` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -43,8 +45,22 @@ CREATE TABLE `felhasznalok` (
 --
 
 CREATE TABLE `kategoriak` (
-  `kategoriaNev` varchar(255) NOT NULL,
-  `kategoriaId` int(11) NOT NULL
+  `KategoriaNev` varchar(255) NOT NULL,
+  `ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kosar`
+--
+
+CREATE TABLE `kosar` (
+  `felhaszID` int(255) NOT NULL,
+  `termekID` int(255) NOT NULL,
+  `Ar` double NOT NULL,
+  `Mennyiseg` int(1) NOT NULL DEFAULT 1,
+  `Osszesen` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -54,10 +70,10 @@ CREATE TABLE `kategoriak` (
 --
 
 CREATE TABLE `rendelesek` (
-  `rendelesId` int(11) NOT NULL,
-  `felhaszId` int(11) NOT NULL,
+  `ID` int(11) NOT NULL,
+  `felhaszID` int(11) NOT NULL,
   `rendlesIdopont` date NOT NULL,
-  `rendelesOsszeg` double NOT NULL,
+  `Osszeg` double NOT NULL,
   `felhaszVnev` varchar(255) NOT NULL,
   `felhaszKnev` varchar(255) NOT NULL,
   `felhaszNev` varchar(255) NOT NULL,
@@ -71,12 +87,12 @@ CREATE TABLE `rendelesek` (
 --
 
 CREATE TABLE `termekek` (
-  `termekId` int(11) NOT NULL,
-  `termekNev` varchar(255) NOT NULL,
-  `termekAr` double NOT NULL,
-  `termekKategoria` varchar(255) NOT NULL,
-  `kategoriaId` int(11) NOT NULL,
-  `termekMennyiseg` int(11) NOT NULL
+  `ID` int(11) NOT NULL,
+  `Nev` varchar(255) NOT NULL,
+  `Ar` double NOT NULL,
+  `Kategoria` varchar(255) NOT NULL,
+  `kategoriaID` int(11) NOT NULL,
+  `Mennyiseg` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
@@ -87,27 +103,33 @@ CREATE TABLE `termekek` (
 -- Indexes for table `felhasznalok`
 --
 ALTER TABLE `felhasznalok`
-  ADD PRIMARY KEY (`felhaszId`);
+  ADD PRIMARY KEY (`ID`);
 
 --
 -- Indexes for table `kategoriak`
 --
 ALTER TABLE `kategoriak`
-  ADD PRIMARY KEY (`kategoriaId`);
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `kosar`
+--
+ALTER TABLE `kosar`
+  ADD KEY `felhaszID` (`felhaszID`);
 
 --
 -- Indexes for table `rendelesek`
 --
 ALTER TABLE `rendelesek`
-  ADD PRIMARY KEY (`rendelesId`),
-  ADD KEY `felhaszId` (`felhaszId`);
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `felhaszId` (`felhaszID`);
 
 --
 -- Indexes for table `termekek`
 --
 ALTER TABLE `termekek`
-  ADD PRIMARY KEY (`termekId`),
-  ADD KEY `kategoriaId` (`kategoriaId`);
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `kategoriaId` (`kategoriaID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -117,35 +139,41 @@ ALTER TABLE `termekek`
 -- AUTO_INCREMENT for table `felhasznalok`
 --
 ALTER TABLE `felhasznalok`
-  MODIFY `felhaszId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `kategoriak`
 --
 ALTER TABLE `kategoriak`
-  MODIFY `kategoriaId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `rendelesek`
 --
 ALTER TABLE `rendelesek`
-  MODIFY `rendelesId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `kosar`
+--
+ALTER TABLE `kosar`
+  ADD CONSTRAINT `kosar_ibfk_1` FOREIGN KEY (`felhaszID`) REFERENCES `felhasznalok` (`ID`);
+
+--
 -- Constraints for table `rendelesek`
 --
 ALTER TABLE `rendelesek`
-  ADD CONSTRAINT `rendelesek_ibfk_1` FOREIGN KEY (`felhaszId`) REFERENCES `felhasznalok` (`felhaszId`);
+  ADD CONSTRAINT `rendelesek_ibfk_1` FOREIGN KEY (`felhaszId`) REFERENCES `felhasznalok` (`ID`);
 
 --
 -- Constraints for table `termekek`
 --
 ALTER TABLE `termekek`
-  ADD CONSTRAINT `termekek_ibfk_1` FOREIGN KEY (`kategoriaId`) REFERENCES `kategoriak` (`kategoriaId`);
+  ADD CONSTRAINT `termekek_ibfk_1` FOREIGN KEY (`kategoriaId`) REFERENCES `kategoriak` (`ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
