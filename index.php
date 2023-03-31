9ece2cbe7356e9504b2be8c5b07b59fb2b5f670b
atiker90 <105391705+atiker90@users.noreply.github.com>
2023-03-31 10:42:26 +0200

    első feltöltés

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    include('header.php');
+?>
+<?php
+    include('./Template/_banner-area.php');
+    include('./Template/_top-sale.php');
+    include('./Template/_special-price.php');
+    include('./Template/_banner-ads.php');
+    include('./Template/_new-products.php');
+    include('./Template/_blogs.php');
+?>
+<?php
+    include('footer.php');
+?>
---
+++ templates/_top-sale.php
@@ -0,0 +1,149 @@
+           <!-- Top Sale -->
+           <section id="top-sale">
+            <div class="container py-5">
+              <h4 class="font-rubik font-size-20">Felkapott</h4>
+              <hr>
+              <!-- owl carousel -->
+                <div class="owl-carousel owl-theme">
+                  <div class="item py-2">
+                    <div class="product font-rale">
+                      <a href="#"><img src="../assets/products/1.jpg" alt="product1" class="img-fluid"></a>
+                      <div class="text-center">
+                        <h6>Dísz</h6>
+                        <div class="rating text-warning font-size-12">
+                          <span><i class="fas fa-star"></i></span>
+                          <span><i class="fas fa-star"></i></span>
+                          <span><i class="fas fa-star"></i></span>
+                          <span><i class="fas fa-star"></i></span>
+                          <span><i class="far fa-star"></i></span>
+                        </div>
+                        <div class="price py-2">
+                          <span>5000 Ft</span>
+                        </div>
+                        <button type="submit" class="btn btn-warning font-size-12">Kosárba</button>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="item py-2">
+                    <div class="product font-rale">
+                      <a href="#"><img src="../assets/products/2.jpg" alt="product1" class="img-fluid"></a>
+                      <div class="text-center">
+                        <h6>Dísz</h6>
+                        <div class="rating text-warning font-size-12">
+                          <span><i class="fas fa-star"></i></span>
+                          <span><i class="fas fa-star"></i></span>
+                          <span><i class="fas fa-star"></i></span>
+                          <span><i class="fas fa-star"></i></span>
+                          <span><i class="far fa-star"></i></span>
+                        </div>
+                        <div class="price py-2">
+                          <span>5000 Ft</span>
+                        </div>
+                        <button type="submit" class="btn btn-warning font-size-12">Kosárba</button>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="item py-2">
+                    <div class="product font-rale">
+                      <a href="#"><img src="../assets/products/3.jpg" alt="product1" class="img-fluid"></a>
+                      <div class="text-center">
+                        <h6>Dísz</h6>
+                        <div class="rating text-warning font-size-12">
+                          <span><i class="fas fa-star"></i></span>
+                          <span><i class="fas fa-star"></i></span>
+                          <span><i class="fas fa-star"></i></span>
+                          <span><i class="fas fa-star"></i></span>
+                          <span><i class="far fa-star"></i></span>
+                        </div>
+                        <div class="price py-2">
+                          <span>5000 Ft</span>
+                        </div>
+                        <button type="submit" class="btn btn-warning font-size-12">Kosárba</button>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="item py-2">
+                    <div class="product font-rale">
+                      <a href="#"><img src="../assets/products/4.jpg" alt="product1" class="img-fluid"></a>
+                      <div class="text-center">
+                        <h6>Dísz</h6>
+                        <div class="rating text-warning font-size-12">
+                          <span><i class="fas fa-star"></i></span>
+                          <span><i class="fas fa-star"></i></span>
+                          <span><i class="fas fa-star"></i></span>
+                          <span><i class="fas fa-star"></i></span>
+                          <span><i class="far fa-star"></i></span>
+                        </div>
+                        <div class="price py-2">
+                          <span>5000 Ft</span>
+                        </div>
+                        <button type="submit" class="btn btn-warning font-size-12">Kosárba</button>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="item py-2">
+                    <div class="product font-rale">
+                      <a href="#"><img src="../assets/products/5.jpg" alt="product1" class="img-fluid"></a>
+                      <div class="text-center">
+                        <h6>Dísz</h6>
+                        <div class="rating text-warning font-size-12">
+                          <span><i class="fas fa-star"></i></span>
+                          <span><i class="fas fa-star"></i></span>
+                          <span><i class="fas fa-star"></i></span>
+                          <span><i class="fas fa-star"></i></span>
+                          <span><i class="far fa-star"></i></span>
+                        </div>
+                        <div class="price py-2">
+                          <span>5000 Ft</span>
+                        </div>
+                        <button type="submit" class="btn btn-warning font-size-12">Kosárba</button>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="item py-2">
+                    <div class="product font-rale">
+                      <div class="d-flex flex-column">
+                        <a href="#"><img src="../assets/products/6.jpg" class="img-fluid" alt="pro1"></a>
+                        <div class="text-center">
+                          <h6>Dísz</h6>
+                          <div class="rating text-warning font-size-12">
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="far fa-star"></i></span>
+                          </div>
+                          <div class="price py-2">
+                            <span>5000 Ft</span>
+                          </div>
+                          <button type="submit" class="btn btn-warning font-size-12">Kosárba</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="item py-2">
+                    <div class="product font-rale">
+                      <div class="d-flex flex-column">
+                        <a href="#"><img src="../assets/products/6.jpg" class="img-fluid" alt="pro1"></a>
+                        <div class="text-center">
+                          <h6>Dísz</h6>
+                          <div class="rating text-warning font-size-12">
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="far fa-star"></i></span>
+                          </div>
+                          <div class="price py-2">
+                            <span>5000 Ft</span>
+                          </div>
+                          <button type="submit" class="btn btn-warning font-size-12">Kosárba</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              <!-- !owl carousel -->
+            </div>
+          </section>
+        <!-- !Top Sale -->
