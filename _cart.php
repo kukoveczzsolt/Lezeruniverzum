7032c1038b040bd68cdc0421cb6ff4ae190a3e5c
KZsolt <kukovecz.zsolt@gmail.com>
2023-04-10 19:51:21 +0200

    feltöltés

---
+++ _cart.php
@@ -0,0 +1,100 @@
+        <!-- cart + summary -->
+        <section class="bg-light my-5">
+            <div class="container">
+                <div class="row">
+                    <!-- cart -->
+                    <div class="col-lg-9">
+                        <div class="card border shadow-0">
+                            <div class="m-4">
+                                <h4 class="card-title mb-4">Kosár</h4>
+                                <div class="row gy-3 mb-4">
+                                    <div class="col-lg-5">
+                                        <div class="me-lg-5">
+                                            <div class="d-flex">
+                                                <img src="assets\products\1.jpg" class="border rounded me-3" style="width: 96px; height: 96px;" />
+                                                <div class="">
+                                                    <a href="#" class="nav-link">Dísz 1</a>
+                                                    <p class="text-muted">Valami</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-2 col-sm-6 col-6 d-flex flex-row flex-lg-column flex-xl-row text-nowrap">
+                                        <div class="">
+                                            <div class="mb-3 me-4" style="width: 100px;">
+                                                <input type="number" class="form-control" id="exampleInputPassword1">
+                                            </div>
+                                        </div>
+                                        <div class="">
+                                            <text class="h6">10000 Ft</text> <br />
+                                            <small class="text-muted text-nowrap"> 1500 Ft / db </small>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg col-sm-6 d-flex justify-content-sm-center justify-content-md-start justify-content-lg-center justify-content-xl-end mb-2">
+                                        <div class="float-md-end">
+                                            <a href="#" class="btn btn-light border text-danger icon-hover-danger">Törlés</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row gy-3 mb-4">
+                                    <div class="col-lg-5">
+                                        <div class="me-lg-5">
+                                            <div class="d-flex">
+                                                <img src="assets\products\2.jpg" class="border rounded me-3" style="width: 96px; height: 96px;" />
+                                                <div class="">
+                                                    <a href="#" class="nav-link">Dísz 2</a>
+                                                    <p class="text-muted">Valami</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-2 col-sm-6 col-6 d-flex flex-row flex-lg-column flex-xl-row text-nowrap">
+                                        <div class="">
+                                            <div class="mb-3 me-4" style="width: 100px;">
+                                                <input type="number" class="form-control" id="exampleInputPassword1">
+                                            </div>
+                                        </div>
+                                        <div class="">
+                                            <text class="h6">1800 Ft</text> <br />
+                                            <small class="text-muted text-nowrap"> 6500 Ft / db </small>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg col-sm-6 d-flex justify-content-sm-center justify-content-md-start justify-content-lg-center justify-content-xl-end mb-2">
+                                        <div class="float-md-end">
+                                            <a href="#" class="btn btn-light border text-danger icon-hover-danger">Törlés</a>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="border-top pt-4 mx-4 mb-4">
+                                <p><i class="fas fa-truck text-muted fa-lg"></i> Ingyenes szállítás</p>
+                                <p class="text-muted">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                    exercitation ullamco laboris nisi ut
+                                    aliquip
+                                </p>
+                            </div>
+                        </div>
+                        <!-- summary -->
+                        <div class="">
+                            <ul class="list-group mb-3">
+                                <li class="list-group-item d-flex justify-content-between">
+                                    <span>Termékek összege</span>
+                                    <strong>100000 Ft</strong>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- summary -->
+                        <div class="float-end">
+                            <button class="btn btn-success shadow-0 border">Tovább</i></button>
+                        </div>
+                    </div>
+                    <!-- cart -->
+                </div>
+            </div>
+        </section>
+        <!-- cart + summary -->
---
+++ cart.php
@@ -0,0 +1,12 @@
+<?php
+include('header.php');
+?>
+
+<?php
+include('./Template/_cart.php');
+include('./Template/_related.php');
+?>
+
+<?php
+include('footer.php');
+?>
