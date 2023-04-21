0bd0b43be9295af3946f95a848928484a0a8d645
KZsolt <kukovecz.zsolt@gmail.com>
2023-04-21 16:00:26 +0200

    filterek hozzáadása

--- template/register.php
+++ template/register.php
@@ -1,4 +1,12 @@
 <?php
+function filter($str)
+{
+  $str = trim($str);
+  $str = strip_tags($str);
+  $str = stripslashes($str);
+  return $str;
+}
+
 session_start();
 include "adatbazisKapcsolat.php";
 $errors = array("email" =>"","jelszo"=>"","jelszo2" => "","vnev" => "","knev" => "");
@@ -14,8 +22,16 @@ $knev;
 #region Üres input mezők ellenőrzése
   if(!empty($_POST["email"]))
   {
-    $email = $_POST["email"];
-    unset($errors["email"]);
+    if($_POST["email"] == filter($_POST["email"]) && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL))
+    {
+      $email = $_POST["email"];
+      unset($errors["email"]);
+    }
+    else
+    {
+      $errors["email"] = "Nem megfelelő email formátum";
+    }
+    
   }
   else
   {
@@ -24,9 +40,16 @@ $knev;
 
   if(!empty($_POST["jelszo"]))
   {
+    if($_POST["jelszo"] == filter($_POST["jelszo"]) && strlen($_POST["jelszo"]) >= 8)
+    {
     $jelszo = $_POST["jelszo"];
     $jelszo_hash = password_hash($jelszo,PASSWORD_DEFAULT);
     unset($errors["jelszo"]);
+    }
+    else
+    {
+      $errors["jelszo"] = "Nem megfelelő jelszó";
+    }
   }
   else
   {
@@ -51,8 +74,15 @@ $knev;
 
   if(!empty($_POST["vnev"]))
   {
-    $vnev = $_POST["vnev"];
-    unset($errors["vnev"]);
+    if(preg_match("/^[a-zA-ZáéíűúőóüöÁÉÍŰÚŐÓÜÖ]*$/u",$_POST["vnev"]))
+    {
+      $vnev = $_POST["vnev"];
+      unset($errors["vnev"]);
+    }
+    else
+    {
+      $errors["vnev"] = "Hibás név formátum";
+    }
   }
   else
   {
@@ -61,8 +91,15 @@ $knev;
 
   if(!empty($_POST["knev"]))
   {
-    $knev = $_POST["knev"];
-    unset($errors["knev"]);
+    if(preg_match("/^[a-zA-ZáéíűúőóüöÁÉÍŰÚŐÓÜÖ]*$/u",$_POST["knev"]))
+    {
+      $knev = $_POST["knev"];
+      unset($errors["knev"]);
+    }
+    else
+    {
+      $errors["knev"] = "Hibás név formátum";
+    }
   }
   else
   {
