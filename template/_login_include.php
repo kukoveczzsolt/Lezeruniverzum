213a87b962b49ac3ecd9f363e1c7454ea0c2a5dd
KZsolt <kukovecz.zsolt@gmail.com>
2023-04-21 16:34:47 +0200

    Rengeteg hiba javítása, optimalizálás

--- template/_login_include.php
+++ template/_login_include.php
@@ -2,20 +2,33 @@
 include "adatbazisKapcsolat.php";
 $errors = array("email" => "","jelszo" => "");
 
-if (isset($_POST["login"]))
+if(isset($_POST["login"]))
 {
-    $email = $_SESSION["email"] = $_POST["email"];
+    $email = $_POST["email"];
     $jelszo = $_POST["jelszo"];
     $parancs = "SELECT * FROM felhasznalok WHERE email='$email'";
     $query = mysqli_query($conn,$parancs);
     $num = mysqli_num_rows($query); 
 
-    #region Üres mezők ellenőrzése/Létező email
     if(!empty($email))
     {
         if($num > 0)
         {
+            $result = mysqli_fetch_array($query);
             unset($errors["email"]);
+            if(password_verify($jelszo,$result["jelszo"]))
+            {
+                unset($errors["jelszo"]);
+                $_SESSION["email"] = $_POST["email"];
+            }
+            elseif(!empty($jelszo))
+            {
+                $errors["jelszo"] = "Hibás jelszó/email";
+            }
+            else
+            {
+                $errors["jelszo"] = "Üres mező";
+            }
         }
         else
         {
@@ -27,34 +40,13 @@ if (isset($_POST["login"]))
         $errors["email"] = "Üres mező";
     }
 
-    if(!empty($jelszo))
-    {
-        if($num > 0)
-        {
-            $result = mysqli_fetch_array($query);
-            if(password_verify($jelszo,$result["jelszo"]))
-            {
-                unset($errors["jelszo"]);
-            }
-            else
-            {
-                $errors["jelszo"] = "Hibás jelszó/email";
-            }
-        }
-        
-    }
-    else
-    {
-        $errors["jelszo"] = "Üres mező";
-    }
-     #endregion
+    
 
     if(count($errors) == 0)
     {
         $query = mysqli_query($conn,$parancs);
         $result = mysqli_fetch_array($query);
         $_SESSION["admin_e"] = $result["admin_e"];
-        echo '<script>alert("Sikeres bejelentkezés")</script>';
         header("Location: index.php");
     }
 }
