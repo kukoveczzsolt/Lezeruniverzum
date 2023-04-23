63ac1311f3ba97f4e0e84d1d7dc5fe5c4c3ef877
KZsolt <kukovecz.zsolt@gmail.com>
2023-04-23 15:59:49 +0200

    font

--- footer.php
+++ footer.php
@@ -16,6 +16,7 @@
 
 
 <!-- Bootstrap 5 JavaScript -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous">
 </script>
 
