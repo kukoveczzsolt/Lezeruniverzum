af96e0de8f2ff4bd6c974685586a129bdec6f1cd
KZsolt <kukovecz.zsolt@gmail.com>
2023-04-24 20:31:35 +0200

    a

--- admin_panel_sql.php
+++ admin_panel_sql.php
@@ -1,4 +1,18 @@
 <?php
-include "adatbazisKapcsolat.php";
+function tabla_feltoltes()
+{
+    include "adatbazisKapcsolat.php";
+    $parancs = "SELECT nev,ar,kategoria FROM termekek";
+    $result = mysqli_query($conn,$parancs);
+    while($row = mysqli_fetch_array($result))
+    {
+        echo "<tr>";
+        echo "<td>".$row["nev"]."</td>";
+        echo "<td>".$row["kategoria"]."</td>";
+        echo "<td>".$row["ar"]." Ft</td>";
+        echo "<td><button type='button' class='btn btn-warning'><i class='bi bi-pencil-square'></i></button> <button type='button' class='btn btn-danger'><i class='bi bi-trash-fill'></i></button> </td>";
+        echo "</tr>";
+    }
+}
 
 ?>
