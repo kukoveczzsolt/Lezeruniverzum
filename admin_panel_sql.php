aae061015b6173ce04e63052ed6363e6c72ac333
KZsolt <kukovecz.zsolt@gmail.com>
2023-04-25 15:03:36 +0200

    termék hozzáadás

--- admin_panel_sql.php
+++ admin_panel_sql.php
@@ -4,13 +4,16 @@ function tabla_feltoltes()
     include "adatbazisKapcsolat.php";
     $parancs = "SELECT nev,ar,kategoria FROM termekek";
     $result = mysqli_query($conn,$parancs);
+    $termekek = array();
     while($row = mysqli_fetch_array($result))
     {
         echo "<tr>";
         echo "<td>".$row["nev"]."</td>";
         echo "<td>".$row["kategoria"]."</td>";
         echo "<td>".$row["ar"]." Ft</td>";
-        echo "<td><button type='button' class='btn btn-warning'><i class='bi bi-pencil-square'></i></button> <button type='button' class='btn btn-danger'><i class='bi bi-trash-fill'></i></button> </td>";
+        echo "";
+        echo "<form action='edit.php' method='post'><td><button type='submit' class='btn btn-warning' name='edit'><i class='bi bi-pencil-square'></i></form>  <form action='delete.php' method='post'></button> <button type='submit' class='btn btn-danger' name='delete' value='".$row["nev"]."'><i class='bi bi-trash-fill'></i></button></td></form>";
+        echo "";
         echo "</tr>";
     }
 }
--- template/_admin_panel.php
+++ template/_admin_panel.php
@@ -2,7 +2,7 @@
 include "admin_panel_sql.php";
 ?>
 
-<div class="container bg-light">
+<div class="container bg-light w-50">
     <div class="table-responsive">
       <table class="table table-striped">
         <thead>
@@ -18,6 +18,6 @@ include "admin_panel_sql.php";
       </table>
     </div>
     <form action="termek_hozzaadasa.php">
-    <button type="submit" class="btn btn-primary " name="uj_termek">Új termék hozzáadása</button>
+      <button type="submit" class="btn btn-primary " name="uj_termek">Új termék hozzáadása</button>
     </form>
 </div>
