cf2c630bea56b859a90531127230701179c4b39e
KZsolt <kukovecz.zsolt@gmail.com>
2023-04-25 17:33:54 +0200

    feltöltése

---
+++ footer.php
@@ -0,0 +1,25 @@
+
+</main>
+
+<footer class="py-3 my-4">
+  <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-black">ÁSZF</a></li>
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-black">Szállítási feltételek</a></li>
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-black">Impresszum</a></li>
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-black">Adatkezelési tájékoztató</a></li>
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-black">Adatvédelmi nyilatkozat</a></li>
+  </ul>
+  <p class="text-center text-black">&copy; 2023 Lézer Univerzum</p>
+</footer>
+</div>
+
+
+
+<!-- Bootstrap 5 JavaScript -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous">
+</script>
+
+</body>
+
+</html>
