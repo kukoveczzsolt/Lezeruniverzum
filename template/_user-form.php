ee6f9e2aaff2e51f8850f679a5e3bd21a7160334
KZsolt <kukovecz.zsolt@gmail.com>
2023-04-30 22:07:13 +0200

    feltöltés

--- template/_user-form.php
+++ template/_user-form.php
@@ -1,82 +1,26 @@
+
 <div class="text-center my-5">
     <h2>Felhasználó adatai</h2>
 </div>
-
 <div class="row my-5">
     <div class="col-4"></div>
     <div class="col-4">
-        <form>
+        <form method="post" action="includes/user_form_include.php">
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email cím</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="<?php echo $row["email"] ?>">
             </div>
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Vezetéknév</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <input type="text" name="vnev" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="<?php echo $row["vnev"] ?>">
             </div>
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Keresztnév</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <input type="text" name="knev" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="<?php echo $row["knev"] ?>">
             </div>
-        </form>
     </div>
     <div class="col-4"></div>
 </div>
-
-<div class="text-center my-5">
-    <h2>Számlázási adatok</h2>
-</div>
-
-<div class="row">
-    <div class="col-4"></div>
-    <div class="col-4">
-        <form>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Város</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Cím 1. sora</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Cím 2. sora</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            </div>
-            <div class="form-check mb-3">
-                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault1" />
-                <label class="form-check-label" for="flexCheckDefault1">Számlázási és szállítási cím megegyezik</label>
-            </div>
-        </form>
-    </div>
-    <div class="col-4"></div>
-</div>
-
-<div class="text-center my-5">
-    <h2>Szállítási adatok</h2>
-</div>
-
-<div class="row">
-    <div class="col-4"></div>
-    <div class="col-4">
-        <form>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Város</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Cím 1. sora</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Cím 2. sora</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            </div>
-        </form>
-    </div>
-    <div class="col-4"></div>
-</div>
-
 
 <div class="text-center my-5">
     <h2>Jelszó módosítása</h2>
@@ -84,17 +28,14 @@
 <div class="row">
     <div class="col-4"></div>
     <div class="col-4">
-        <form>
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Új jelszó</label>
-                <input type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <input type="password" class="form-control" name="jelszo" id="exampleInputEmail1" aria-describedby="emailHelp">
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Új jelszó megerősítése</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <input type="password" class="form-control" name="jelszo2" id="exampleInputPassword1">
             </div>
-
-        </form>
     </div>
     <div class="col-4"></div>
 </div>
@@ -105,13 +46,13 @@
 <div class="row">
     <div class="col-4"></div>
     <div class="col-4">
-        <form>
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Jelszó</label>
-                <input type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <input type="password" name="jelszo_kuldes" class="form-control mb-2" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <p class="text-danger"><?php echo $_SESSION["error"]; unset($_SESSION["error"]); ?></p>
             </div>
-            <button type="submit" class="btn btn-primary float-end">Mentés</button>
-        </form>
+            <button type="submit" class="btn btn-primary float-end" name="kuldes">Mentés</button>
     </div>
     <div class="col-4"></div>
+    </form>
 </div>
